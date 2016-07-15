8fcd8b704187f6ac6878a337baca4fa6bc797f95
Jonham <jonham@qq.com>
2016-07-15 08:56:15 +0800

    basic setup

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+
+var info;
+var target = JH("#info");
+JH.on(window, "deviceorientation", function(e) {
+    // console.log(e);
+    // target.innerHTML = "Alpha" + e.alpha +
+    //               "<br> beta:" + e.beta +
+    //               "<br> gamma:" + e.gamma;
+    // info = e;
+    mouseX = e.alpha - 180;
+    mouseY = e.beta;
+});
